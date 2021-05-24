acf7f5cae3c7398b744fb4b573c2976cb4ef9be1
abidwalkar <abidwalkar@deakin.edu.au>
2021-05-25 09:30:59 +1000

    Add templates dir

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py migrate --no-input
+release: python manage.py loaddata dump.json
 web: gunicorn Property_Portal.wsgi --log-file -
--- Property_Portal/settings.py
+++ Property_Portal/settings.py
@@ -56,7 +56,7 @@ WSGI_APPLICATION = 'wsgi.application'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "prediction/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
